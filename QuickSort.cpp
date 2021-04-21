b144b947401a0031daefd6195ab8ba2931a26551
Cyan75 <incanto075@gmail.com>
2021-04-22 00:47:25 +0900

    sementation fault: 11

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -6,29 +6,62 @@
 class QuickSort
 {
 private:
-    short pivotIndex;
     std::vector<short> dataArray;
+    void print(void)
+    {
+        for (auto i : dataArray)
+        {
+            std::cout << i << ", ";
+        }
+    }
 
 public:
     QuickSort()
     {
-        pivotIndex = 2;
         dataArray = {5, 4, 3, 2, 1};
+        /*
+        TODO:
+        */
+        partition(dataArray);
+        print();
     }
+
     void partition(std::vector<short> &vec)
     {
-        while (vec.size() > 1)
+        if (vec.size() > 1)
         {
+            /*
+            TODO : set pivot index
+            */
+            short pivotIndex = 2;
             std::vector<short> leftV(vec.begin(), vec.begin() + pivotIndex - 1);
             std::vector<short> rightV(vec.begin() + pivotIndex + 1, vec.end());
-            std::vector<short> buffer;
-            for (short i = 0; i < pivotIndex; ++i)
+            for (auto iL : leftV)
             {
-                if (vec[i] > vec[pivotIndex])
+                if (iL > vec[pivotIndex])
                 {
+                    rightV.push_back(iL);
+                    leftV.erase(leftV.begin() + iL);
                 }
             }
-            partition(vec);
+            for (auto iR : rightV)
+            {
+                if (iR < vec[pivotIndex])
+                {
+                    leftV.push_back(iR);
+                    rightV.erase(rightV.begin() + iR);
+                }
+            }
+            partition(leftV);
+            partition(rightV);
+        }
+        else
+        {
         }
     }
 };
+int main(void)
+{
+    QuickSort q;
+    return 1;
+}
