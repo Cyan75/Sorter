800f86646c7fd5f04f99011d6e0b4bf2c90bdacf
Cyan75 <incanto075@gmail.com>
2021-04-25 21:25:52 +0900

    using vector and pair leads to the same error - Segmentation fault: 11

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <time.h>
+#include <utility>
 
 class QuickSort
 {
@@ -22,22 +23,24 @@ public:
         /*
         TODO:
         */
+
+        partition(std::make_pair(0, dataArray.size()));
         print();
     }
-    std::pair<short, short> partition(std::pair<short, short> partRange) //lower bound, upper bound
+    void partition(std::pair<short, short> partRange) //lower bound, upper bound
     {
-
-        short &lowerBound = std::get<0>(partRange);
-        short &upperBound = std::get<1>(partRange);
+        short lowerBound = std::get<0>(partRange);
+        short upperBound = std::get<1>(partRange);
         short pivotIndex = 2; //get pivot index(between lower bound and upper bound)
+        short pivotValue = dataArray[pivotIndex];
         std::vector<short> leftV(dataArray.begin() + lowerBound, dataArray.begin() + pivotIndex - 1);
         std::vector<short> rightV(dataArray.begin() + pivotIndex + 1, dataArray.begin() + upperBound);
-
+        dataArray.clear();
         if (!leftV.empty())
         {
             for (auto iL : leftV)
             {
-                if (iL > dataArray[pivotIndex])
+                if (iL > pivotValue)
                 {
                     rightV.push_back(iL);
                     leftV.erase(leftV.begin() + iL);
@@ -49,58 +52,24 @@ public:
         {
             for (auto iR : rightV)
             {
-                if (iR < dataArray[pivotIndex])
+                if (iR < pivotValue)
                 {
                     leftV.push_back(iR);
                     rightV.erase(rightV.begin() + iR);
                 }
             }
         }
-        partition(std::make_pair(lowerBound + 1, pivotIndex - 1));
-        partition(return std::make_pair(pivotIndex + 1, upperBound - 1));
+        dataArray = leftV;
+        dataArray.push_back(pivotValue);
+        dataArray.insert(dataArray.end(), rightV.begin(), rightV.end());
+
+        //partition(std::make_pair(lowerBound + 1, pivotIndex - 1));
+        //partition(std::make_pair(pivotIndex + 1, upperBound - 1));
     }
     //or
     /* 
     passing information about the indexes of begining, dividing, and ending point in the previous partitioning
     */
-    std::tuple<short, short, short>
-    partition(std::tuple<short, short, short> idxBeginPivotEnd)
-    {
-        /*
-            TODO : set pivot index
-        */
-        //short pivotIndex = std::get<1>(beginPivotEnd);
-        //vector::end()  is past-the-end iterator
-        std::vector<short> leftV(dataArray.begin() + std::get<0>(idxBeginPivotEnd), dataArray.begin() + std::get<1>(idxBeginPivotEnd) - 1);
-        std::vector<short> rightV(dataArray.begin() + std::get<1>(idxBeginPivotEnd) + 1, dataArray.begin() + std::get<2>(idxBeginPivotEnd));
-        if (!leftV.empty())
-        {
-            for (auto iL : leftV)
-            {
-                if (iL > dataArray[std::get<1>(idxBeginPivotEnd)])
-                {
-                    rightV.push_back(iL);
-                    leftV.erase(leftV.begin() + iL);
-                }
-            }
-        }
-        if (!rightV.empty())
-        {
-            for (auto iR : rightV)
-            {
-                if (iR < dataArray[std::get<1>(idxBeginPivotEnd)])
-                {
-                    leftV.push_back(iR);
-                    rightV.erase(rightV.begin() + iR);
-                }
-            }
-        }
-        /*
-        short beg = 
-        short end = 
-        return std::maketuple(beg, pivot, end);
-        */
-    }
 };
 int main(void)
 {
