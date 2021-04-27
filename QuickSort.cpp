ebd45e2601b98d568f3f3d7895660efe279732e6
Cyan75 <incanto075@gmail.com>
2021-04-27 23:56:17 +0900

    unit partitioning successful. need to introduce some iteration with a nested design.

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -4,17 +4,27 @@
 #include <string>
 #include <time.h>
 #include <utility>
+#include <algorithm>
 
 class QuickSort
 {
 private:
     std::vector<short> dataArray;
-    void print(void)
+    void print(std::vector<short> &vec)
     {
-        for (auto i : dataArray)
+        for (auto i : vec)
         {
             std::cout << i << ", ";
         }
+    }
+    std::vector<short> reconstruct(std::vector<short> &leftV, std::vector<short> &rightV, short pivot)
+    {
+        //this->dataArray.clear();
+        std::vector<short> vec;
+        vec.insert(vec.end(), leftV.begin(), leftV.end());
+        vec.push_back(pivot);
+        vec.insert(vec.end(), rightV.begin(), rightV.end());
+        return vec;
     }
 
 public:
@@ -25,48 +35,31 @@ public:
         TODO:
         */
 
-        partition();
-        print();
+        partition(dataArray);
+        print(dataArray);
     }
-    void partition() //lower bound, upper bound
+    void partition(std::vector<short> &vec)
     {
-        //vector
         short lowerBound = 0;
-        short upperBound = this->dataArray.size();
-        short pivotIndexValue = 5; //get pivot index(between lower bound and upper bound)
+        short upperBound = vec.size();
+        short pivotIndexValue = upperBound / 2; //get pivot index(between lower bound and upper bound)
 
-        std::vector<short>::iterator pivot = dataArray.begin();
+        std::vector<short>::iterator pivot = vec.begin();
         advance(pivot, pivotIndexValue - 1);
 
-        std::cout << "begin : " << *dataArray.begin() << std::endl;
+        //std::cout << "begin : " << *dataArray.begin() << std::endl;
         //std::cout << "end : " << *dataArray.end() << std::endl;//0
         std::cout << "pivot : " << *pivot << std::endl;
-        std::cout << "dataArray size: " << dataArray.size() << std::endl;
+        //std::cout << "dataArray size: " << dataArray.size() << std::endl;
 
-        std::vector<short> leftV(dataArray.begin(), pivot);
-        std::cout << "leftV before part : ";
-        for (auto i : leftV)
-        {
-            std::cout << i << ", ";
-        }
-        std::cout << std::endl;
+        std::vector<short> leftV(vec.begin(), pivot);
+        std::vector<short> rightV(pivot + 1, vec.end());
 
-        std::vector<short> rightV(pivot + 1, dataArray.end());
-        std::cout << "rightV before part : ";
-        for (auto i : rightV)
-        {
-            std::cout << i << ", ";
-        }
-        std::cout << std::endl;
-
-        //partition---------------------------------------------------
-        //this->dataArray.clear();
         if (!leftV.empty())
         {
             std::vector<short>::iterator iL = leftV.begin();
             for (auto i : leftV)
             {
-                std::cout << "*iL = " << *iL << std::endl;
                 if (*iL > *pivot)
                 {
                     rightV.push_back(*iL);
@@ -80,14 +73,13 @@ public:
         }
         else
         {
-            std::cout << "leftV is empty : " << std::endl;
+            std::cout << "leftV is empty!" << std::endl;
         }
         if (!rightV.empty())
         {
             std::vector<short>::iterator iR = rightV.begin();
             for (auto i : rightV)
             {
-                std::cout << "*iR = " << *iR << std::endl;
                 if (*iR < *pivot)
                 {
                     leftV.push_back(*iR);
@@ -101,32 +93,12 @@ public:
         }
         else
         {
-            std::cout << "rightV is empty " << std::endl;
+            std::cout << "rightV is empty" << std::endl;
         }
-        std::cout << "leftV after part : ";
-        for (auto i : leftV)
-        {
-            std::cout << i << ", ";
-        }
-        std::cout << std::endl;
-        std::cout << "rightV after part : ";
-        for (auto i : rightV)
-        {
-            std::cout << i << ", ";
-        }
-        std::cout << std::endl;
-
-        /* this->dataArray = leftV;
-        this->dataArray.push_back(pivotValue);
-        this->dataArray.insert(dataArray.end(), rightV.begin(), rightV.end());
- */
-        //partition(std::make_pair(lowerBound + 1, pivotIndex - 1));
-        //partition(std::make_pair(pivotIndex + 1, upperBound - 1));
+        reconstruct(leftV, rightV, *pivot);
+        partition(leftV);
+        partition(rightV);
     }
-    //or
-    /* 
-    passing information about the indexes of begining, dividing, and ending point in the previous partitioning
-    */
 };
 int main(void)
 {
